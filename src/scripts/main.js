5a8d4cb13d2a76cd02af0779ca6100d92245e37c
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-03-20 11:14:38 -0500

    Finished business, lightning exercises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -29,7 +29,7 @@ let zipCodeProperty = "addressZipCode"
 // loop through each item in array, check condition (addressStateCode === "NY") if true, keep, if false, disregard
 
 // filter method - loops through every single item in the array, and takes a callback function as the argument; primary differences betwen filter and forEach is it returns a NEW array; must return boolean
-// current array is business; using business.filter will give new array and will be needed to be assigned to a variable 
+// current array is business; using business.filter will give new array and will be needed to be assigned to a variable
 // attempting to return a boolean - if true, return, if false, disregard; essentially checking if business.addressStateCode === "NY". If it is, it returns true. If not, it returns false. For every item(object) where the address code does not equal NY, it is not added to the array. If it is equal to NY, it is added to the array. True: in array. False: not in array.
 
 // Array to contain all the New York businesses
@@ -63,29 +63,76 @@ let zipCodeProperty = "addressZipCode"
 //     outEl.innerHTML += "<hr/>"
 //   });
 
-const manufacturingBusinesses = businesses.filter(business => {
-    let manufacturer = false
-    if (business.companyIndustry === "Manufacturing") {
-        manufacturer = true
-    }
-    return manufacturer
+// const manufacturingBusinesses = businesses.filter(business => {
+//     let manufacturer = false
+//     if (business.companyIndustry === "Manufacturing") {
+//         manufacturer = true
+//     }
+//     return manufacturer
+// })
+
+// manufacturingBusinesses.forEach(business => {
+//     outEl.innerHTML += `
+//       <h2>${business.companyName}</h2>
+//       <section>
+//         ${business.addressFullStreet}
+//       </section>
+//       <section>
+//           ${business.addressCity}
+//           ${business["addressStateCode"]}
+//       </section>
+//       <section>
+//           ${business[zipCodeProperty]}
+//       </section>
+//     `
+//     outEl.innerHTML += "<hr/>"
+//   });
+
+// console.log(manufacturingBusinesses)
+
+// businesses.forEach(business => {
+//     /* CALCULATE ORDER SUMMARY */
+// let totalOrders = business.orders.reduce(
+//     (currentTotal, nextValue) => currentTotal += nextValue,
+//     0
+// )
+
+    /* CALCULATE ORDER SUMMARY */
+    // let totalOrders = 0
+    // business.orders.forEach(order => totalOrders += order)
+// loops through each array in business, and then second forEach loops through orders (a property within the array), places it within the variable totalOrders, and the following code appends it to the DOM
+
+//     outEl.innerHTML += `
+//         <h2>
+//             ${business.companyName}
+//             ($${totalOrders.toFixed(2)})
+//         </h2>
+//         <section>
+//             ${business.addressFullStreet}
+//         </section>
+//         <section>
+//             ${business.addressCity},
+//             ${business.addressStateCode}
+//             ${business.addressZipCode}
+//         </section>
+//     `;
+//     outEl.innerHTML += "<hr/>";
+// });
+
+// lighting exercises
+
+const monthlyRainfall = [23, 13, 27, 20, 20, 31, 33, 26, 19, 12, 14, 12, 10]
+
+monthlyRainfall.forEach(amount => {
+    const totalRainfall = monthlyRainfall.reduce(
+    (accumulator, currentRainfall) => accumulator += currentRainfall) 
+    console.log(totalRainfall)
+
 })
 
-manufacturingBusinesses.forEach(business => {
-    outEl.innerHTML += `
-      <h2>${business.companyName}</h2>
-      <section>
-        ${business.addressFullStreet}
-      </section>
-      <section>
-          ${business.addressCity}
-          ${business["addressStateCode"]}
-      </section>
-      <section>
-          ${business[zipCodeProperty]}
-      </section>
-    `
-    outEl.innerHTML += "<hr/>"
-  });
+const words = ["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
 
-console.log(manufacturingBusinesses)
+const sentence = words.reduce((fullSentence, currentWord) => fullSentence + " " + currentWord)
+
+console.log(sentence)
+
